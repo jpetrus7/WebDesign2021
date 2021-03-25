34afbc7bf950fd68ba98e0c2502fac09c8a9c0d9
Petrus <josh.petrus21@bcp.org>
2021-03-25 11:05:12 -0700

    Jetpack Joyride
    Understanding the code

---
+++ jpetrus/_js/jetpack.js
@@ -0,0 +1,46 @@
+// Created by Joshua Petrus
+
+// Creating a sprite with animations
+var intr = createSprite(200, 200);
+intr.setAnimation("animation_2_2");
+
+var help = createSprite(242, 243);
+help.setAnimation("help");
+help.visible = false;
+
+var pett = createSprite(200, 200,10,10);
+
+var fruits = createSprite(340, 135);
+fruits.setAnimation("an");
+var Xz = createSprite(287,315);
+Xz.setAnimation("C");
+Xz.visible = false;
+fruitsbackground.visible = false;
+
+
+var fruit1 = createSprite(137, 120,5,5);
+fruit1.visible = false;
+fruit1.setAnimation("fruit1");
+
+var fruit2 = createSprite(200, 200,5,5);
+fruit2.visible = false;
+fruit2.setAnimation("fruit2");
+
+
+var fruit4 = createSprite(196, 267,5,5);
+fruit4.visible = false;
+fruit4.setAnimation("fruit4");
+
+var fruit5 = createSprite(275, 122,5,5);
+fruit5.visible = false;
+fruit5.setAnimation("fruit5");
+
+
+
+ var gww = createSprite(200, 200);
+ gww.setAnimation("animation_2_copy_1");
+gww.visible = false;
+
+var spr = createSprite(200, 200);
+spr.setAnimation("animation_2");
+spr.visible = true;
